6e2b6ae3d7a5276ae6fe1ffc0e092c3435d36e04
DMC <surendra.singh20.in@gmail.com>
2024-11-27 08:14:32 +0700

    Update ExceptionListHis.java

--- src/main/java/com/gl/eirs/hlrdeactivation/entity/app/ExceptionListHis.java
+++ src/main/java/com/gl/eirs/hlrdeactivation/entity/app/ExceptionListHis.java
@@ -58,7 +58,7 @@ public class ExceptionListHis {
     @Column(name="tac")
     String tac;
 
-    @Column(name="remarks")
+    @Column(name="remark")
     String remarks;
 
     @Column(name="operation")
